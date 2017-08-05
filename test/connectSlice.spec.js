3c90a061bb52a9c96d6b6fa4afda96218a814b9a
Heber Jairo Lopez Zalba <heberlopez@gmail.com>
2017-08-04 17:57:50 -0700

    more tests added (#5)

--- test/connectSlice.spec.js
+++ test/connectSlice.spec.js
@@ -141,4 +141,74 @@ describe('Connect Slice Reducers Tests', () => {
 
     expect(sillyDOM.text()).toBe(new SampleReducerClass().start().message);
   });
+
+  it('checks actions get correctly injected into component', () => {
+    const SillyComp = ({ message, start }) => <div id="silly" onClick={start}>{message}</div>;
+    class SampleReducerClass {
+      start = () => ({ message: 'hello', started: true });
+    }
+
+    const SampleReducer = sliceReducer('sample')(SampleReducerClass);
+    const reducer = combineSliceReducers(SampleReducer);
+    const store = createStore(reducer);
+    const SampleActions = generateActionDispatchers(store.dispatch, 'start');
+
+    const Container = connectSlice(
+      { slice: 'sample', actions: SampleActions }
+    )(SillyComp);
+
+    const providerWrapper = mount(<Provider store={store}><Container /></Provider>);
+    const sillyDOM = providerWrapper.find('#silly');
+    sillyDOM.simulate('click');
+
+    expect(sillyDOM.text()).toBe(new SampleReducerClass().start().message);
+  });
+
+  it('checks component only re-render when their slice or props changed', () => {
+    class SillyComp1 extends React.Component {
+      render() {
+        const { message, start } = this.props;
+        return <div><div id="silly1" onClick={start}>{message}</div><Container2 /></div>;
+      }
+    }
+    class SillyComp2 extends React.Component {
+      render() {
+        const { message } = this.props;
+        return <div id="silly2">{message}</div>;
+      }
+    }
+
+    class SampleReducerClass1 {
+      start = () => ({ message: 'hello', started: true });
+    }
+    class SampleReducerClass2 {}
+
+    const SampleReducer1 = sliceReducer('sample1')(SampleReducerClass1);
+    const SampleReducer2 = sliceReducer('sample2')(SampleReducerClass2);
+    const reducer = combineSliceReducers(SampleReducer1, SampleReducer2);
+    const store = createStore(reducer);
+    const SampleActions = generateActionDispatchers(store.dispatch, 'start');
+
+    const Container1 = connectSlice(
+      { slice: 'sample1', actions: SampleActions }
+    )(SillyComp1);
+
+    const Container2 = connectSlice(
+      { slice: 'sample2' }
+    )(SillyComp2);
+
+    const silly1RenderSpy = jest.spyOn(SillyComp1.prototype, 'render');
+    const silly2RenderSpy = jest.spyOn(SillyComp2.prototype, 'render');
+    expect(silly1RenderSpy).toHaveBeenCalledTimes(0);
+    expect(silly2RenderSpy).toHaveBeenCalledTimes(0);
+
+    const providerWrapper = mount(<Provider store={store}><Container1 /></Provider>);
+    expect(silly1RenderSpy).toHaveBeenCalledTimes(1);
+    expect(silly2RenderSpy).toHaveBeenCalledTimes(1);
+
+    providerWrapper.find('#silly1').simulate('click');
+    expect(silly1RenderSpy).toHaveBeenCalledTimes(2);
+    expect(silly2RenderSpy).toHaveBeenCalledTimes(1);
+  });
+
 });
